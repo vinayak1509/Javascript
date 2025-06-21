ed5c451d4e782056e8cf10072b0a6644df7593d9
vinayak1509 <vinayak.77thakur@gmail.com>
2025-06-21 17:15:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3) we are using nodejs not browser
+
+
+console.log(3+ // code readability should be high
+    3) 
+
+
+
+let name = "vinayak"
+let age = 20
+let isLoggedin = false
+let state = null
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =>standalone value
+// undefined =>
+// symbol => to find uniqueness
+
+
+// objects
+
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
