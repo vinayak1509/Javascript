e16d335963a7c14945910338501ed38b673145cd
vinayak1509 <vinayak.77thakur@gmail.com>
2025-06-22 02:32:14 +0000

    done

---
+++ 01_basics/comparisions.js
@@ -0,0 +1,14 @@
+//console.log(2>1);
+console.log("2" > 1); // string is converted into number in both cases
+console.log("02" > 1); 
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+console.log(undefined > 0); // false
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
+
+
+console.log(2==="2"); // === cpmapres value as well as data types
