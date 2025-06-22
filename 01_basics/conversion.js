26d0caf5b517949fb61f465819a1a6ccd0f2f239
vinayak1509 <vinayak.77thakur@gmail.com>
2025-06-22 02:05:11 +0000

    conversions done

---
+++ 01_basics/conversion.js
@@ -0,0 +1,24 @@
+let score = 33;
+//console.log(score);
+let name = "33abc"
+//console.log(typeof score);
+let valueInName = Number(name);
+//console.log(typeof valueInName); // number
+//console.log( valueInName); // NaN
+//console.log( name);
+
+// "33"=>33
+// "33abc" => NaN
+// true =>1 / false =>0
+
+
+let isLoggedIn = "VINAYAK";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+ // 1 => TRUE // 0 => false
+ // " "=>fasle
+ // "vinayak" =>true
+
+let some = 33;
+let stringsome = String(some);
+console.log(typeof stringsome);
