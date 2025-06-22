9eab2d8a13c7be7ae65c5a9a5c4f4598793ce29b
vinayak1509 <vinayak.77thakur@gmail.com>
2025-06-22 02:43:15 +0000

    datatypes done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,12 @@
+// Primitive 
+
+// 7 types : String ,Number , boolean, null, undefined, Symbol , Bigint
+
+
+// java Script is a dynamically typed language
+
+// Refernce (Non primitive)
+
+// Array, Objects, Functions
+// type of non primitive data types is object
+// type of bigint is Undefined
