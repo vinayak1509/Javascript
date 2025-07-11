1ea17f4e46530df5ad0e5240186346dec6542831
vinayak1509 <vinayak.77thakur@gmail.com>
2025-07-11 18:14:15 +0000

    functions done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -20,3 +20,36 @@ function userloginMessage(username){
 }
 // console.log(userloginMessage()); undefined just logged in
 // function userloginMessage(username = "sam") -----> if nothing is passed then sam is printed in place of username
+
+
+
+function calculateCartPrice(...num1){ // (...) is rest operator, it converts all the values into an array
+    return num1;
+}
+
+// console.log(calculateCartPrice(2,2,3,4,5)); [ 2, 2, 3, 4, 5 ]
+
+function calculatePrice(val1, val2, ...num1){
+    return num1;
+}
+// console.log(calculatePrice(10,2, 4,5,6)); [ 4, 5, 6 ] 10 goes to val1 and 2 goes to val2 rest is converted to arrya num1
+
+
+const user = {
+    username : "vinayak",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+// handleObject(user)
+
+
+// handleObject({
+//     username:"sama",
+//     price:100
+// })  you can directly pass the object definition in function
+
+
