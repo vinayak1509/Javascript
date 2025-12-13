ed8fbb06732b9d67ff6ee7fa38d8dfc4864c6a29
vinayak1509 <vinayak.77thakur@gmail.com>
2025-12-13 23:06:42 +0530

    Update string.js

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -1,7 +1,7 @@
 const name = "vinayak"
 const repoCount = 50
-console.log(name + repoCount + "value");
-console.log(`hello my name is ${name} and my repoCount is ${repoCount}`);
+console.log(name + repoCount + "value"); //vinayak50value
+console.log(`hello my name is ${name} and my repoCount is ${repoCount}`); //hello my name is vinayak and my repoCount is 50
 const gameName  =new  String("vinayak-kackd-kaefe-tg");
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
@@ -9,10 +9,10 @@ const gameName  =new  String("vinayak-kackd-kaefe-tg");
 // console.log(gameName.indexOf('n'));
 
 const newString  = gameName.substring(0,4) // return chars from 0th index to 3rd index
-console.log(newString);
+console.log(newString);  //vina
 
-const anotherString = gameName.slice(-9,5)
-console.log(anotherString);
+const anotherString = gameName.slice(0,5) 
+console.log(anotherString); // vinay : returns a portion of string
 
 const newString1 = "       vinayak  " // trims the white spaces
 console.log(newString1.trim());
